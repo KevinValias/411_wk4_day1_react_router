a4810cbb8b65f5c4362f96b428d862a0c97fae61
Kevin Valias <Kevbodyworks@gmail.com>
2020-10-04 09:15:35 -0500

    Finsihed Product

--- src/Router.js
+++ src/Router.js
@@ -1,17 +1,29 @@
-/* 
-    Write code for React Router here
-Import the Home and About components from 
-the component folder. Assign the default
-path "/" to the Home component and "/about"
-to the About component.
-
-Remember to export the component at the end
-*/
-
 import React from 'react'
 import { Switch, Route } from 'react-router'
-// Write component imports here //
+import Home from './components/Home';
+import About from './components/About';
+import Car from './components/Car'
 
 
 
-// Start Router function here //
+ 
+function Router() {
+    return(
+        <div>
+            <Switch>
+                <Route exact path='/' component={Home}/>
+                <Route path='/about' component={About}/>
+                <Route path='/car/:id' component=
+                {Car}/>
+            </Switch>
+        </div>
+    );
+}
+        
+
+
+
+
+
+
+export default Router;
--- src/components/Car.js
+++ src/components/Car.js
@@ -1,12 +1,25 @@
 import React from 'react'
 import cars from '../cars.json'
+import { Container, Paper, Chip } from '@material-ui/core';
 // import material ui components here //
 // Container, Paper, Chip //
 
 const Car = (props) => {
+    const id = props.match.params.id
+    const car = cars.find(car => id == car.id)
     return (
-        <h1>A specific car</h1>
+        
+        <Container className='car-container'>
+            <Paper className='car-paper'>
+        <h1>{car.Name}</h1>
+        {Object.keys(car).map((key) => {
+            return(<Chip label={`${key}: ${car[key]}`}></Chip>)
+        })}
+            </Paper>
+       </Container>
     )
 }
 
-export default Car
+
+
+export default Car;
